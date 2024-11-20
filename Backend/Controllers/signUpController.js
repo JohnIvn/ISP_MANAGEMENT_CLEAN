2a4aab9388535af80b0c1d75f1b7cdc8f6c3e478
Jeffrey Aspiras <you@example.com>
2024-11-20 01:06:15 -0800

    new model and controller for adminaccounts

--- Backend/Controllers/signUpController.js
+++ Backend/Controllers/signUpController.js
@@ -1,23 +1,27 @@
 import db from '../database.js';
 import bcrypt from 'bcrypt';
-import CreateAccountModel from '../Models/signUpModel.js';
+import { UserAccount, AdminAccount } from '../Models/signUpModel.js';
 
 const SignUp = async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 12);
 
     try {
-        const existingAccount = await CreateAccountModel.findOne({ where: { email } });
+        const existingAccount = await UserAccount.findOne({ where: { email } });
 
         if (existingAccount) {
             return res.status(400).json({ message: 'Email must be unique' });
         }
-
-        const newAccount = await CreateAccountModel.create({
+        const newUserAccount = await UserAccount.create({
             firstName,
             lastName,
             email,
             password: hashedPassword, 
+        });
+        const newAdminAccount = await AdminAccount.create({
+            userID: newUserAccount.userId,  
+            email, 
+            password
         });
 
         return res.status(201).json({ message: 'Account created successfully' });
--- Backend/Models/signUpModel.js
+++ Backend/Models/signUpModel.js
@@ -1,38 +1,76 @@
 import db from '../database.js';
 import { Sequelize, DataTypes, Model } from 'sequelize';
-class CreateAccountModel extends Model {}
 
-CreateAccountModel.init({
+// Define UserAccount Model
+class UserAccountModel extends Model {}
+
+const UserAccount = UserAccountModel.init(
+  {
     userId: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: Sequelize.NOW, 
-    }
-}, {
-    sequelize: db, 
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize: db,
     modelName: 'UserAccount',
-    timestamps: false 
-});
+    tableName: 'useraccounts',
+    timestamps: false, // You can set this to true if you want timestamps
+  }
+);
 
-export default CreateAccountModel;
+// Define AdminAccount Model
+class AdminAccountModel extends Model {}
+
+const AdminAccount = AdminAccountModel.init(
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, 
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'AdminAccount',
+    tableName: 'adminaccounts',
+    timestamps: false, 
+  }
+);
+
+export { UserAccount, AdminAccount };
